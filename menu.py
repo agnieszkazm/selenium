5882b6c108de8fc4ee2f5b83200b1267c2aa93d1
agnieszkazm <agnieszka.rydz@gmail.com>
2017-11-14 14:20:18 +0100

    clean up

--- menu.py
+++ menu.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 @pytest.fixture
 def driver(request):
     browser=webdriver.Firefox()
-    # print(browser.capabilities)
+    #print(browser.capabilities)
     request.addfinalizer(browser.quit)
     return browser
 
@@ -20,27 +20,18 @@ def test_loggin(driver):
     #assert "My Store" in driver.title
     assert wait.until(EC.title_is("My Store"))
 
-    #table = driver.find_element_by_id("box-apps-menu")
-    apps = driver.find_elements_by_css_selector("ul#box-apps-menu li")
-    doc = 0
-    for i in range(len(apps)):
-        #table = driver.find_element_by_id("box-apps-menu")
-        #doc = driver.find_elements_by_css_selector('ul#box-apps-menu ul li')
-        app1=driver.find_elements_by_css_selector("ul#box-apps-menu li")[i+doc].click()
-        doc = len(driver.find_elements_by_css_selector('ul#box-apps-menu ul li'))
-        naglowek1 = driver.find_element_by_css_selector("h1").text
-        print(naglowek1)
-        #app1.click()
+    menulenght = driver.find_elements_by_css_selector("ul#box-apps-menu li")
+    submenulength = 0
+    for i in range(len(menulenght)):
+        menu = driver.find_elements_by_css_selector("ul#box-apps-menu li")[i+submenulength].click()
+        submenulength = len(driver.find_elements_by_css_selector('ul#box-apps-menu ul li'))
+        header1 = driver.find_element_by_css_selector("h1").text
+        print(header1)
 
+        for j in range(submenulength):
+            submenu = driver.find_element_by_css_selector("ul#box-apps-menu ul li:nth-child(" + str(j + 1) + ")").click()
+            header2 = driver.find_element_by_css_selector("h1").text
+            print(header2)
 
-       # if doc > 0:
-        for j in range(doc):
-            doc1 = driver.find_element_by_css_selector("ul#box-apps-menu ul li:nth-child(" + str(j + 1) + ")").click()
-            #table = driver.find_element_by_id("box-apps-menu")
-            # apps = table.find_elements_by_tag_name("li")[i]
-            #driver.find_element_by_css_selector("ul#box-apps-menu ul li:nth-child(" + str(j + 1) + ")").click()
-            naglowek2 = driver.find_element_by_css_selector("h1").text
-            print(naglowek2)
-            #doc1.click()
 
 
