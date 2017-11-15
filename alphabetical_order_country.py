642ec4e48a09350963adf046c63453f97b2437b8
agnieszkazm <agnieszka.rydz@gmail.com>
2017-11-15 19:20:41 +0100

    adding time zones

--- alphabetical_order_country.py
+++ alphabetical_order_country.py
@@ -21,7 +21,7 @@ def test_alphabetical_order(driver):
     #assert "My Store" in driver.title
     assert wait.until(EC.title_is("My Store"))
 
-    mainMenu = driver.find_element_by_css_selector("ul#box-apps-menu li:nth-child(3)").click()
+    mainMenuCountry = driver.find_element_by_css_selector("ul#box-apps-menu li:nth-child(3)").click()
     assert wait.until(EC.url_contains("http://localhost/litecart/admin/?app=countries&doc=countries"))
 
 #Creating list
@@ -49,8 +49,8 @@ def test_alphabetical_order(driver):
         sublistCheckelement = driver.find_elements_by_css_selector("[name=countries_form] td:nth-child(6)")[i].get_attribute("textContent")
         if sublistCheckelement != "0":
             driver.find_elements_by_css_selector("[name=countries_form] td:nth-of-type(5) a")[i].click()
-    #creating sublist
 
+#creating sublist
             subcountryListLenght = driver.find_elements_by_css_selector("#table-zones td:nth-child(3)")
             subcountryList = []
             for j in range(len(subcountryListLenght)):
@@ -58,7 +58,7 @@ def test_alphabetical_order(driver):
                 subcountryList.append(subcountryName)
                 print(subcountryName)
 
-     #checking alphabetical order
+#checking alphabetical order
             for j in range(len(subcountryList)-1):
                 if subcountryList[j] > subcountryList[j+1]:
                     result2 = False
@@ -69,4 +69,35 @@ def test_alphabetical_order(driver):
                 assert result2 == True
 
             driver.execute_script("window.history.go(-1)")
+
+#checking zones alphabetical order
+    mainMenuZone = driver.find_element_by_css_selector("ul#box-apps-menu li:nth-child(6)").click()
+    assert wait.until(EC.url_contains("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones"))
+
+#creating zones list
+    zonesListlength = driver.find_elements_by_css_selector(".dataTable td:nth-child(3) a")
+    for i in range(len(zonesListlength)):
+        driver.find_elements_by_css_selector(".dataTable td:nth-child(3) a")[i].click()
+        assert wait.until(EC.url_contains("http://localhost/litecart/admin/?app=geo_zones&doc=edit_geo_zone&page=1&geo_zone_id="))
+
+#creating list of selected time zone
+        subZonesListLength = driver.find_elements_by_css_selector("#table-zones td:nth-child(3) select [selected=selected]")
+        subZonesList = []
+        for i in range (len(subZonesListLength)):
+            subCheckZone = driver.find_elements_by_css_selector("#table-zones td:nth-child(3) select [selected=selected]")[i].text
+            subZonesList.append(subCheckZone)
+            print(subCheckZone)
+
+#checking alphabetical order
+        for i in range(len(subZonesList)-1):
+            if subZonesList[i] > subZonesList[i+1]:
+                result3 = False
+                break
+            else:
+                result3 = True
+
+            assert result3 == True
+
+        driver.execute_script("window.history.go(-1)")
+
 
