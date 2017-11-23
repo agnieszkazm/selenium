4cf37b8d31037842bb1fe89ac3b17f847f438b06
agnieszkazm <agnieszka.rydz@gmail.com>
2017-11-23 23:14:15 +0100

    store test v3

--- store_test.py
+++ store_test.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 
 
@@ -25,18 +25,24 @@ def test_store(driver):
         driver.find_elements_by_css_selector("#box-most-popular li")[i].click()
         #wait.until(EC.title_is("http://localhost/litecart/en/rubber-ducks-c-1/subcategory-c-2/yellow-duck-p-1"))
         try:
-            addArticelArt = driver.find_element_by_css_selector('options [name="options[Size]"]').click()
-            addArticelArtDropdown = driver.find_element_by_css_selector('options [name="options[Size]"] [value="Medium"]')
+            addArticelArt = driver.find_element_by_css_selector('.options [name="options[Size]"]').click()
+            addArticelArtDropdown = driver.find_element_by_css_selector('.options [name="options[Size]"] [value="Medium"]').click()
         except:
             pass
 
-        cartCounterLocator = '#cart .quantity'
-        cartCounter = driver.find_element_by_css_selector(cartCounterLocator)
+
+        cartCounter = driver.find_element_by_css_selector('#cart .quantity')
         addToCart= driver.find_element_by_css_selector(".quantity [type='submit']").click()
-        wait.until(EC.text_to_be_present_in_element(cartCounterLocator, str(i+1)))
+        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,'#cart .quantity'), str(i+1)))
         cartCounter
-        assert(cartCounter == 1+i)
+        assert(cartCounter.text == str(1+i))
 
         driver.execute_script("window.history.go(-1)")
 
-
+    basket = driver.find_element_by_css_selector("#cart .link").click()
+    assert wait.until(EC.title_is("Checkout | My Store"))
+    for i in range(3):
+        remove_button = driver.find_elements_by_css_selector("#box-checkout-cart [name='remove_cart_item']")[0]
+        #wait.until(EC.visibility_of(remove_button))
+        remove_button.click()
+        wait.until(EC.staleness_of(driver.find_element_by_css_selector('.dataTable')))
