2a196414f0fcceca3446352bad8876a865cf09e3
agnieszkazm <agnieszka.rydz@gmail.com>
2018-01-16 22:55:45 +0100

    cleaning

--- menu_behave/steps/menu.py
+++ menu_behave/steps/menu.py
@@ -72,35 +72,14 @@ def step_impl(context, element):
 def step_impl(context, input_field, text):
     quantity = context.driver.find_element_by_css_selector(input_field).send_keys(text)
 
-
-    #date_from = context.driver.find_element_by_css_selector("tbody [name=date_valid_from]").send_keys("11/17/2017" + Keys.TAB + "12/31/2017")
-
-# Information tab
-
-    #info_tab = context.driver.find_element_by_css_selector('form [href="#tab-information"]').click()
-    #manufacturer = context.driver.find_element_by_css_selector('tbody [name="manufacturer_id"]').click()
-    #select_manufact = context.driver.find_element_by_css_selector('tbody [name="manufacturer_id"] option:last-child').click()
- @When (u"I fill '{text} and '{text}' and '{text}' and '{text}' and '{text}' in '{input_field}' input field") 
- def step_ impl(context, text, input_field): 
+@When (u"I fill '{text} and '{text}' and '{text}' and '{text}' and '{text}' in '{input_field}' input field") 
+def step_ impl(context, text, input_field): 
     text_fields = context.driver.find_element_by_css_selector(input_field).send_keys(text + Keys.TAB + text + Keys.TAB + text + Keys.TAB + text + Keys.TAB + text)
 
-# Prices tab
-
-    #info_tab = context.driver.find_element_by_css_selector('form [href="#tab-prices"]').click()
 @When(u"I delete existing element and add new '{text}' in '{input_field}' input field")
 def step_impl(context, text, input_field):
     purchase_price = context.driver.find_element_by_css_selector(input_field).send_keys(Keys.DELETE + text)
-    
-    #currency_click = context.driver.find_element_by_css_selector('tbody [name="purchase_price_currency_code"]').click()
-    #currency = context.driver.find_element_by_css_selector('tbody [name="purchase_price_currency_code"] option:last-child').click()
-    #gross_usd = context.driver.find_element_by_css_selector('#tab-prices [name="gross_prices[USD]"]').send_keys(Keys.DELETE + "30")
-    #gross_eur = context.driver.find_element_by_css_selector('#tab-prices [name="gross_prices[EUR]"]').send_keys(Keys.DELETE + "25")
-    #time.sleep(2)
-# Save product
-    #save = context.driver.find_element_by_css_selector('.button-set [name=save]').click()
-    #time.sleep(3)
-# Check if product is added
+
 @Then(u"I can see '{id_element}' element on page")
 def step_impl(context, id_element)
     assert context.driver.find_element_by_css_selector(id_element)
-    #time.sleep(3)tab-prices [name="gross_prices[EUR]"]
